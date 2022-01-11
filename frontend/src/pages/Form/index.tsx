913aa8e7a56eb1f4953763bade4826052dd7347b
Leonardo Ribeiro <leonardo@ericom.com.br>
2022-01-11 00:23:47 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form(){
+
+    return(
+        <h1>Pagina Formulário</h1>
+
+
+    );
+}
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing(){
+
+    return(
+        <h1>Pagina listagem</h1>
+
+
+    );
+}
+export default Listing;
