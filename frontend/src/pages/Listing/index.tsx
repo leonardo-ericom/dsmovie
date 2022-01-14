675f671b11907eed3b861e05046d053b9c2b91e3
Leonardo Ribeiro <leonardo@ericom.com.br>
2022-01-14 20:15:21 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,21 +1,24 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movies";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
- 
- 
- //FORMA ERRADA 
- axios.get(`${BASE_URL}/movies?size=12&page=0`)
- .then(response =>{
-   console.log(response.data);
+  const [pageNumber, setPageNumber] = useState(0);
 
- });
- 
- 
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      const data = response.data as MoviePage;
+     console.log(data);
+      setPageNumber(data.number);
+    });
+  }, []);
+
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
@@ -37,10 +40,12 @@ function Listing() {
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          
         </div>
       </div>
     </>
   );
 }
 export default Listing;
+function userState(): [any, any] {
+  throw new Error("Function not implemented.");
+}
