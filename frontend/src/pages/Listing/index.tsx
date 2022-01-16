e71d694c09f6908d52cbe62c5f9cd08b4903ec1f
Leonardo Ribeiro <leonardo@ericom.com.br>
2022-01-16 17:07:20 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -28,15 +28,17 @@ function Listing() {
         setPage(data);
       });
   }, [pageNumber]);
-    
+
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  };
+
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
       <div className="container">
         <div className="row">
-
-
-          {page.content.map(movie => (
+          {page.content.map((movie) => (
             <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
               <MovieCard movie={movie} />
             </div>
