74f83bd070c193a993f2d1a33aada83e83c24df1
Leonardo Ribeiro <leonardo@ericom.com.br>
2022-01-15 23:17:37 -0300

    userParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,28 +22,21 @@ function Listing() {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
       .then((response) => {
         const data = response.data as MoviePage;
         setPage(data);
       });
   }, [pageNumber]);
-
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
-
+    
   return (
     <>
       <Pagination />
       <div className="container">
         <div className="row">
-          {page.content.map((movie) => (
+
+
+          {page.content.map(movie => (
             <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
               <MovieCard movie={movie} />
             </div>
